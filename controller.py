9e2333b53dcdda501160609067c4970c0b53da8c
aleannox <th@thomas-hack.net>
2021-08-18 07:20:28 +0100

    Add dry run mode.

--- controller.py
+++ controller.py
@@ -23,9 +23,13 @@ class TankController:
     else:
         URL_BASE = 'http://octopi.local/api/'
     
-    def __init__(self):
-        TankController._wait_for_api()
-        TankController._connect_api()
+    def __init__(self, dry_run):
+        self.dry_run = dry_run
+        if self.dry_run:
+            logger.info("Dry run, skipping api connection.")
+        else:
+            TankController._wait_for_api()
+            TankController._connect_api()
         self._init_gun()
         self._init_turret()
         self._init_chains()
@@ -74,7 +78,7 @@ class TankController:
             
     def move_chains(self, position, speed):
         logger.info(f"Moving chains to E{position} with F{speed}.")
-        TankController._send_gcode(f'G0 E{position} F{speed}')
+        self._send_gcode(f'G0 E{position} F{speed}')
         # Expected duration for this move, we sleep for this duration.
         # Currently we do not know of any way to query whether the printer is actually moving.
         duration = abs(position - self.chain_position) / 1000 * self.config['chain_time_per_1000']
@@ -88,7 +92,9 @@ class TankController:
         # We only use it to add sound-flavor to the other movements.
         # For this a self-contained random movement is sufficient.
         # Clip gun movement so it does not take too long.
-        diff = random.randint(-self.config['gun_max_per_move'], self.config['gun_max_per_move'])
+        diff = random.randint(
+            -self.config['gun_max_per_move'], self.config['gun_max_per_move']
+        )
         target = max(
             min(
                 self.gun_position + diff,
@@ -98,8 +104,10 @@ class TankController:
         )
         diff = target - self.gun_position
         duration = abs(diff) / 1000 * self.config['gun_time_per_1000']
-        logger.info(f"Moving gun to E{target} with F{self.config['gun_speed']}.")
-        TankController._send_gcode(f'G0 X{target} F{self.config["gun_speed"]}')
+        logger.info(
+            f"Moving gun to E{target} with F{self.config['gun_speed']}."
+        )
+        self._send_gcode(f'G0 X{target} F{self.config["gun_speed"]}')
         logger.info(f"Expected move duration {duration:.01f}s")
         time.sleep(duration)
         self.gun_position = target
@@ -107,17 +115,17 @@ class TankController:
     def _init_gun(self):
         # TODO: homing, currently homing sensor is not at 0 but max ~= 2000
         logger.info("Initializing gun.")
-        TankController._send_gcode('G92 X0')
+        self._send_gcode('G92 X0')
         self.gun_position = 0
 
     def _init_turret(self):
         # just disable, hopefully preventing thermal runaway
         logger.info("Initializing turrent.")
-        TankController._send_gcode('M18 Z')
+        self._send_gcode('M18 Z')
         
     def _init_chains(self):
         logger.info("Initializing chains.")
-        TankController._send_gcode(
+        self._send_gcode(
             [
                 'M163 S0 P0.5',
                 'M163 S1 P0.5',
@@ -127,16 +135,18 @@ class TankController:
             ]
         )
         self.chain_position = 0
-        
-    @staticmethod
-    def _send_gcode(commands):
+
+    def _send_gcode(self, commands):
         if isinstance(commands, str):
             commands = [commands]
-        logger.info(f"Sending GCODE commands: {commands}")
-        result = TankController._api_post(
-            'printer/command', json_={'commands': commands}
-        )
-        assert result.status_code == 204, f"Sent wrong GCODE command {commands}."
+        if self.dry_run:
+            logger.info(f"[Dry run] Sending GCODE commands: {commands}")
+        else:
+            logger.info(f"Sending GCODE commands: {commands}")
+            result = TankController._api_post(
+                'printer/command', json_={'commands': commands}
+            )
+            assert result.status_code == 204, f"Sent wrong GCODE command {commands}."
         
     @staticmethod
     def _wait_for_api():
--- run.py
+++ run.py
@@ -1,15 +1,31 @@
+import argparse
 import logging
 
 import controller
 
 
-def main():
+def main(dry_run):
     logging.basicConfig(level=logging.INFO)
 
-    t = controller.TankController()
-    # t.run_random()
+    t = controller.TankController(dry_run)
     t.run_look_at_person()
 
 
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--dry-run',
+        dest='dry_run',
+        action='store_true',
+        help=(
+            'Run without connection to the Octopi API, '
+            'tank commands are only logged but not submitted.'
+        )
+    )
+    parser.set_defaults(dry_run=False)
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    main()
+    arguments = get_arguments()
+    main(arguments.dry_run)
